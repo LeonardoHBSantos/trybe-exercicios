b6c3950e17b70da30a776d8d8c9c6bd593f79e0a
LeonardoHBSantos <leosantos1720@gmail.com>
2023-01-25 12:03:10 -0300

    Concluido exercico Para Fixar 2

---
+++ Fundamentos/Secao 03 - Introducao a JavaScript/01 - JavaScript -Primeiros Passos/script exercicio 2.js
@@ -0,0 +1,6 @@
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+const perimeter = base + base + heigth + heigth;
+
+console.log('A área do retângulo é de ' + area + ' e o seu perímetro e de ' + perimeter + '.');
