6f46636c09be0235012e4757a26ebe24230c23ac
LeonardoHBSantos <leosantos1720@gmail.com>
2023-01-25 10:36:41 -0300

    Concluido exercico Para Fixar 1

---
+++ Fundamentos/Secao 03 - Introducao a JavaScript/01 - JavaScript -Primeiros Passos/script exercicio 1.js
@@ -0,0 +1,9 @@
+const MyName = 'Leonardo';
+const birthCity = 'Minas Novas - MG';
+let birthYear = 2003;
+birthYear = 2030;
+birthCity = 'Belo Horizonte - MG'
+
+console.log(MyName);
+console.log(birthCity);
+console.log(birthYear);
